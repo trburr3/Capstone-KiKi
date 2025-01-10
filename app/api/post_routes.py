e1a74cc33c3af709e7464fe053f9e5d59f0eae3d
Tre <trburrows@syr.edu>
2025-01-09 22:16:17 -0500

    comment routes added to posts

--- app/api/post_routes.py
+++ app/api/post_routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint, jsonify, request
 from app.models.db import db
-from app.models import User, Post
+from app.models import User, Post, Comment
 from flask_login import current_user, login_required
 from datetime import datetime
 
@@ -79,3 +79,65 @@ def delete_post(post_id):
     db.session.delete(post)
     db.session.commit()
     return { 'Post': 'Post successfully deleted.' }
+
+@post_routes.route('/<int:post_id>/comments')
+@login_required
+def get_all_comments(post_id):
+    comments = Comment.query.filter(Comment.post_id == post_id).order_by(Comment.id.desc())
+    if not comments :
+        return { 'errors': { 'Comments': 'No comments found.' } }, 404
+    def commentNormalizer(comment):
+        formattedComment = {
+            "id": comment.id,
+            "author_id": comment.author_id,
+            "author_name": User.query.get(comment.author_id).first_name + ' ' + User.query.get(comment.author_id).last_name,
+            "auhtor_pic": User.query.get(comment.author_id).prof_pic,
+            "comment": comment.comment,
+            "created_at": comment.created_at,
+            "updated_at": comment.updated_at,
+        }
+        return formattedComment
+    return {'Comments': [commentNormalizer(comment) for comment in comments]}
+
+@post_routes.route('/<int:post_id>/comments', methods=['POST'])
+@login_required
+def create_comment(post_id):
+    user_id = int(current_user.get_id())
+    data = request.get_json()
+
+    newComment = Comment(
+        author_id = user_id,
+        post_id = post_id,
+        comment = data.get('comment'),
+    )
+
+    db.session.add(newComment)
+    db.session.commit()
+    return newComment.to_dict(), 201
+
+@post_routes.route('/comments/<int:comment_id>', methods=['PUT'])
+@login_required
+def edit_comment(comment_id):
+    user_id = int(current_user.get_id())
+    target_comment = Comment.query.get(comment_id)
+    if not target_comment or not target_comment.author_id == user_id:
+        return { 'errors': { 'Comment': 'No comment found.' } }, 404
+
+    data = request.get_json()
+    target_comment.comment = data.get("comment")
+
+    target_comment.updated_at = datetime.now().strftime("%m/%d/%Y, %H:%M:%S")
+    db.session.commit()
+    return target_comment.to_dict()
+
+@post_routes.route('/comments/<int:comment_id>', methods=['DELETE'])
+@login_required
+def delete_comment(comment_id):
+    user_id = int(current_user.get_id())
+    comment = Comment.query.get(comment_id)
+    if not comment or not comment.author_id == user_id:
+        return { 'errors': { 'Comment': 'No comment found.' } }, 404
+
+    db.session.delete(comment)
+    db.session.commit()
+    return { 'Comment': 'Comment successfully deleted.' }
